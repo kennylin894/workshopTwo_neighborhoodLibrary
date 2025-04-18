3ccaa7d9b89fe97b84ac79169057ae27adc7b4e5
unknown <klin@gmail.com>
2025-04-18 11:58:41 -0400

    added some comments, fixed up interface a lil bit

--- src/main/java/com/ps/commands.java
+++ src/main/java/com/ps/commands.java
@@ -7,11 +7,14 @@ import java.util.Scanner;
 public class commands {
     static Scanner scanner = new Scanner(System.in);
     static Inventory inv = new Inventory();
+
+    //user chooses option 1 - These are the available books
     public static void option1()
     {
         System.out.println("These are the available books");
         System.out.println(printAvailableBooks(availableBooks(inv)));
-        if(option1B() == 1)
+        //user chooses to check out
+        if(option1B().equals("c"))
         {
             System.out.println("Please enter your name:");
             scanner.nextLine();
@@ -19,10 +22,14 @@ public class commands {
             System.out.println("What book would you like to check out, enter the book id");
             int id = scanner.nextInt();
             chooseBook(name,id);
+        }
+        else if(option1B().equals("x"))
+        {
 
         }
     }
 
+    //checks what books are available in the inventory
     public static ArrayList<Book> availableBooks(Inventory inventory)
     {
         StringBuilder string = new StringBuilder();
@@ -37,6 +44,7 @@ public class commands {
         return available;
     }
 
+    //printer method to print all the books
     public static StringBuilder printAvailableBooks(ArrayList<Book> available)
     {
         StringBuilder string = new StringBuilder();
@@ -53,16 +61,17 @@ public class commands {
         return string;
     }
 
-
-    public static int option1B()
+    //user chooses to check out the books
+    public static String option1B()
     {
         System.out.println("Would you like to:");
-        System.out.println("1) Select a book to check out");
-        System.out.println("2) Exit the application");
-        int input = scanner.nextInt();
+        System.out.println("C) Select a book to check out");
+        System.out.println("X) Exit the application");
+        String input  = scanner.nextLine().toLowerCase();
         return input;
     }
 
+    //used when the user wants to select a book
     public static void chooseBook(String name,int id)
     {
         for(Book book: availableBooks(inv))
@@ -84,18 +93,30 @@ public class commands {
     }
 
 
+    //user chooses to show the books that are checked out
     public static void option2()
     {
         System.out.println("These are all the checked out books");
         System.out.println(printCheckOutBooks(notAvailableBooks(inv)));
+        //user wants to check in a book
         if(option2B().equals("c"))
         {
             System.out.println("What book would you like to check in, enter the book id");
             int id = scanner.nextInt();
             chooseCheckIn(id);
         }
+        //user chooses to exit the application
+        else if(option2B().equals("x"))
+        {
+            System.out.println("Good Bye.");
+        }
+        else
+        {
+            System.out.println("Error, bad input. Please try again");
+        }
     }
 
+    //lets user choose the
     public static String option2B()
     {
         System.out.println("Choose an option");
