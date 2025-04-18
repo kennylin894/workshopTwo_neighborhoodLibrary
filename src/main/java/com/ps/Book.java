ea4906454f8447e4c4f167bef131ad6df2ca7e7c
unknown <klin@gmail.com>
2025-04-17 22:41:14 -0400

    finished classes I think

--- src/main/java/com/ps/Book.java
+++ src/main/java/com/ps/Book.java
@@ -1,12 +1,38 @@
 package com.ps;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Book {
     private int id;
     private String isbn;
     private String title;
     private Boolean isCheckedOut;
-    private String isCheckedOutTo;
+    private String CheckedOutTo;
+    private ArrayList<String> inventory;
+    public Book()
+    {
 
+    }
+    public void checkOut(String name)
+    {
+        this.CheckedOutTo = name;
+        this.isCheckedOut = true;
+    }
+
+    public ArrayList<String> getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(ArrayList<String> inventory) {
+        this.inventory = inventory;
+    }
+
+    public void checkIn()
+    {
+        this.CheckedOutTo = "";
+        this.isCheckedOut = false;
+    }
 
     public String getIsbn() {
         return isbn;
@@ -40,11 +66,11 @@ public class Book {
         isCheckedOut = checkedOut;
     }
 
-    public String getIsCheckedOutTo() {
-        return isCheckedOutTo;
+    public String getCheckedOutTo() {
+        return CheckedOutTo;
     }
 
-    public void setIsCheckedOutTo(String isCheckedOutTo) {
-        this.isCheckedOutTo = isCheckedOutTo;
+    public void setCheckedOutTo(String isCheckedOutTo) {
+        this.CheckedOutTo = isCheckedOutTo;
     }
 }
