a4ea54e87f9d6a86839df3010934e601c78c3e32
unknown <klin@gmail.com>
2025-04-17 22:53:17 -0400

    basic interface

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -1,7 +1,47 @@
 package com.ps;
 
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to Library HomeScreen");
+        System.out.println("1) Show available books");
+        System.out.println("2) Show checked out books");
+        System.out.println("3) Check in a book");
+        System.out.println("4) Exit application");
+        int input = scanner.nextInt();
+
+
+        if(input == 1)
+        {
+            System.out.println("These are the available books");
+
+        }
+        else if(input == 2)
+        {
+            System.out.println("Choose an option");
+            System.out.println("C) to check in a book");
+            System.out.println("X) go back to home screen");
+        }
+        else if(input == 3)
+        {
+            System.out.println("Choose an option");
+            System.out.println("C) to check in a book");
+            System.out.println("X) go back to home screen");
+
+        }
+        else if(input == 4)
+        {
+            System.out.println("Good bye.");
+        }
+        else
+        {
+            System.out.println("Error, invalid input");
+        }
+
 
     }
 }
